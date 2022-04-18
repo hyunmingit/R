fdd0097de38710f29a7a9112f109e443794e31b4
unknown <jhmcos13@gmail.com>
2022-04-18 15:43:23 +0900

    220418 HelloR

---
+++ Ch01/1_HelloR.R
@@ -0,0 +1,18 @@
+#20220418
+#장현민
+#Hello R
+
+print("Hello R")
+print('안녕 R')
+
+#대입연산자
+hello <- "Hello World"
+print(hello)
+
+welcome <- 'Welcome R!'
+welcome
+
+greeting <- 'Greeting R!'
+greeting
+
+
---
+++ Ch01/3_자료형.R
@@ -0,0 +1,27 @@
+#20220418
+#장현민
+#자료형 실습하기
+
+#숫자형(정수, 실수)
+num1 <- 1
+num2 <- -2
+num3 <- 3.14
+num1;num2;num3
+
+#논리형
+bool1 <- TRUE
+bool2 <- FALSE
+bool1
+bool2
+
+#문자형(문자, 문자열)
+str1 <- 'Hello'
+str2 <- '안녕'
+str1;str2
+ 
+#특수형
+var1 <- NULL #값이 없음을 의미하는 값
+var2 <- NA #결측값, Not Available
+var3 <- NaN #수학적으로 정의가 불가능 한 값, Not a Number
+var1;var2;var3
+
